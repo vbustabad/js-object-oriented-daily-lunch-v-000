5696f192026523603e929b8f15af33cd0b767705
Veronica Bustabad <vbustabad@gmail.com>
2018-09-13 22:14:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ class Customer {
 
       static totalSpent() {
           return this.meals().reduce(function(agg, el, i, arr) {
-            return agg + el.revenue;
+            return agg += el.revenue;
           }, 0);
       }
   }
